822db948be33d33ce3c8b5b5db5efafeed957ea9
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 11:17:25 +0700

    ok

--- test_ocr.py
+++ test_ocr.py
@@ -8,7 +8,7 @@ import glob
 
 config = Cfg.load_config_from_name('vgg_transformer')
 config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
-config['device'] = 'cuda:0'
+config['device'] = 'cpu'
 config['predictor']['beamsearch'] = False
 
 detector = Predictor(config)
