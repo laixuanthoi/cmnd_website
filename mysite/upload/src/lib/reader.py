4bb9cb0f8f3575da85f51edbc29dc4f4534a58f4
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 09:47:15 +0700

    ok

--- mysite/upload/src/lib/reader.py
+++ mysite/upload/src/lib/reader.py
@@ -1,61 +1,17 @@
-import pytesseract
-import cv2
-import numpy as np
-import re
-pytesseract.pytesseract.tesseract_cmd = "D:/tesseract-ocr/tesseract.exe"
+import matplotlib.pyplot as plt
+from PIL import Image
 
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
 
-class READER:
-    def textClean(self, text):
-        vietnamese_text = r'[a-z0-9A-Z_àáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+'
-        regex = re.compile(vietnamese_text)
-        match = re.findall(regex, text)
-        return match
+config = Cfg.load_config_from_name('vgg_transformer')
+config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
+config['device'] = 'cuda:0'
+config['predictor']['beamsearch'] = False
 
-    def readText(self, image):
-        config = '--psm 13'
-        lang = 'vie_best'
-        return pytesseract.image_to_string(image, lang=lang, config=config)
-
-    def readNumber(self, image):
-        config = '--psm 13'
-        lang = 'vie_best'
-        return pytesseract.image_to_string(image, lang=lang, config=config)
-
-    def readMultiText(self, images):
-        texts = []
-        for img in images:
-            text = self.readText(img)
-            matchs = self.textClean(text)
-            if len(matchs) == 0:
-                text = "#"
-            else:
-                text = ''.join(matchs)
-            texts.append(text)
-        return texts
-
-    def readMultiNumber(self, images):
-        texts = []
-        for img in images:
-            text = self.readNumber(img)
-            matchs = self.textClean(text)
-            if len(matchs) == 0:
-                text = "#"
-            else:
-                text = ''.join(matchs)
-            texts.append(text)
-        return texts
-
-    def readNguyenQuan(self, boxes):
-        full_text = ''
-        for row in boxes:
-            for img in row:
-                text = self.readText(img)
-                matchs = self.textClean(text)
-                if len(matchs) == 0:
-                    text = "#"
-                else:
-                    text = ''.join(matchs)
-                full_text += '{} '.format(text)
-            full_text += '\n'
-        return full_text
+detector = Predictor(config)
+img = "C:/Users/MSi/Desktop/9.JPG"
+img = Image.open(img)
+plt.imshow(img)
+s = detector.predict(img)
+print(s)
