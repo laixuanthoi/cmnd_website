f1685a161b0bdd8a9eb2c8869c26af6779d70f31
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 10:10:55 +0700

    ok

--- mysite/upload/src/lib/reader.py
+++ mysite/upload/src/lib/reader.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-
 from vietocr.tool.predictor import Predictor
 from vietocr.tool.config import Cfg
 
@@ -8,10 +5,8 @@ config = Cfg.load_config_from_name('vgg_transformer')
 config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
 config['device'] = 'cuda:0'
 config['predictor']['beamsearch'] = False
-
 detector = Predictor(config)
-img = "C:/Users/MSi/Desktop/9.JPG"
-img = Image.open(img)
-plt.imshow(img)
-s = detector.predict(img)
-print(s)
+
+
+def readText(image):
+    return detector.predict(image)
