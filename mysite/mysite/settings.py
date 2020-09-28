62095825408f5b5f3cad80e488eb2fb88fed55e4
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 16:10:41 +0700

    ok

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import socket
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+socke.tconnect("awdawd")
 
 # Application definition
 
