5ec41c9439b47ca36965c5946fe224908fe80e74
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 10:20:46 +0700

    ok

--- test_ocr.py
+++ test_ocr.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
+from vietocr.tool.config import Cfg
+from vietocr.tool.predictor import Predictor
 from PIL import Image
 
-from vietocr.tool.predictor import Predictor
-from vietocr.tool.config import Cfg
+import matplotlib.pyplot as plt
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 
 config = Cfg.load_config_from_name('vgg_transformer')
 config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
@@ -12,6 +14,6 @@ config['predictor']['beamsearch'] = False
 detector = Predictor(config)
 img = "image/17.JPG"
 img = Image.open(img)
-plt.imshow(img)
+# plt.imshow(img)
 s = detector.predict(img)
 print(s)
