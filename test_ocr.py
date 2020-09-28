b7357efc28f54a1bfee2e439a68279904fee5604
laixuanthoi <laixuanthoi@gmail.com>
2020-09-28 11:15:12 +0700

    ok

--- test_ocr.py
+++ test_ocr.py
@@ -1,19 +1,33 @@
 from vietocr.tool.config import Cfg
 from vietocr.tool.predictor import Predictor
+import cv2
 from PIL import Image
-
 import matplotlib.pyplot as plt
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
+import time
+import glob
 
 config = Cfg.load_config_from_name('vgg_transformer')
 config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
-config['device'] = 'cpu'
+config['device'] = 'cuda:0'
 config['predictor']['beamsearch'] = False
 
 detector = Predictor(config)
-img = "image/17.JPG"
-img = Image.open(img)
-# plt.imshow(img)
-s = detector.predict(img)
-print(s)
+
+
+# img = "image/20.JPG"
+# img = cv2.imread(img)
+# pil_img = Image.fromarray(img)
+# start = time.time()
+# s = detector.predict(pil_img)
+# print(s, "predicted: ", time.time()-start)
+# cv2.imshow("image", img)
+# cv2.waitKey(0)
+
+for f in glob.glob("image/*.JPG"):
+    img = cv2.imread(f)
+    pil_img = Image.fromarray(img)
+    start = time.time()
+    s = detector.predict(pil_img)
+    print(s, "predicted: ", time.time()-start)
+    # cv2.imshow("image", img)
+    # cv2.waitKey(0)
