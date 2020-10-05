915ea281a7409594cd8e53e4fa47b9e8bd857cd0
laixuanthoi <laixuanthoi@gmail.com>
2020-10-05 11:45:34 +0700

    ok

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
