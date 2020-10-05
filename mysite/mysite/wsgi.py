b43c6eba71d53a0edbc32921a5eeb070742800c5
laixuanthoi <laixuanthoi@gmail.com>
2020-10-05 11:17:17 +0700

    ok

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,16 +1,25 @@
-"""
-WSGI config for mysite project.
+# """
+# WSGI config for mysite project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+# """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# application = get_wsgi_application()
+
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+import os
+from subprocess import call
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
 application = get_wsgi_application()
+call('python manage.py runserver 127.0.0.1:8000')
